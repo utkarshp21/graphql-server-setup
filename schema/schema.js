58716bf86a38b326b43a9ce23d9c08098574f4a6
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-01-23 14:53:28 -0500

    added comments

--- schema/schema.js
+++ schema/schema.js
@@ -30,6 +30,9 @@ var authors = [
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
+    //We are wrapping fields in the function as we dont want to execute this ultil 
+    //everything is inilized. For example below code will throw error AuthorType not found 
+    //if not wrapped in a function
     fields: () => ({
         id: { type: GraphQLID  },
         name: { type: GraphQLString }, 
