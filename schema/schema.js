238f85f55c0fc59d77cad63d0737b8897ac6e3a5
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-01-22 23:30:09 -0500

    adding author schema

--- schema/schema.js
+++ schema/schema.js
@@ -2,15 +2,22 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLSchema } = graphql;
 
-// Schema defines data on the Graph like object types(book type), relation between these object types
-// and descibes how it can reach into the graph to interact with the data to 
-// retrieve or mutate the data   
+//Schema defines data on the Graph like object types(book type), relation between 
+//these object types and describes how it can reach into the graph to interact with 
+//the data to retrieve or mutate the data   
 
+//Dummy Data
 var books = [
     { name:"Book 1", pages:432 , id:1},
     { name: "Book 2", pages: 32, id: 2},
     { name: "Book 3", pages: 532, id: 3 }
 ]
+
+var authors = [
+    { name: 'Author 1', age: 44, id: 1 },
+    { name: 'Author 2', age: 42, id: 1 },
+    { name: 'Author 3', age: 66, id: 3 }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -21,8 +28,18 @@ const BookType = new GraphQLObjectType({
     })
 });
 
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+})
+
 //RootQuery describe how users can use the graph and grab data.
-//E.g Root query to get all authors, get all books, get a particular book or get a particular author.
+//E.g Root query to get all authors, get all books, get a particular 
+//book or get a particular author.
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -35,6 +52,13 @@ const RootQuery = new GraphQLObjectType({
 
                 //this will return the book with id passed in argument by the user
                 return books.find((item) => { return item.id == args.id});
+            }
+        },
+        author:{
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return authors.find((item) => { return item.id == args.id });
             }
         }
     }
