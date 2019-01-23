a55c3668bc26d697f91d001649731a183cecfb28
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2019-01-22 22:34:55 -0500

    schema setup

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const schema = require('./schema/schema')
 
 const app = express();
 
-//This route will be used as an endpoint to interact with our Graphql, 
-//A ll our GraphQL queries will be routed through this route. 
+//This route will be used as an endpoint to interact with Graphql, 
+//All queries will go through this route. 
 app.use('/graphql', graphqlHTTP({
-    schema
+    //telling express-graphql to use this schema to map out the graph 
+    schema,
+    //telling express-graphql to use graphiql when goto '/graphql' address in the browser
+    //which provides an interface to make GraphQl queries
+    graphiql:true
 }));
 
-app.listen(6000, () => {
-    console.log('Listening on port 6000');
-});
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+}); 
